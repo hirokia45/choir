22243f96b0f955054f207a55751f4e7ae29f1fe5
hirokia45 <hiroki.amada.90@gmail.com>
2023-03-14 03:24:46 +0900

    date bug fix

--- slices/PerformanceHistory/index.tsx
+++ slices/PerformanceHistory/index.tsx
@@ -11,30 +11,31 @@ import {
  * @typedef {import("@prismicio/react").SliceComponentProps<PerformanceHistorySlice>} PerformanceHistoryProps
  * @param { PerformanceHistoryProps }
  */
-const PerformanceHistory = ({ slice }: { slice: PerformanceHistorySlice }) => (
-  <section className="pt-4 sm:pt-8 pb-8 sm:pb-12">
-    <div className="text-center p-6">
-      <div className="text-lg font-medium text-gray-900">
-        Performance History / 演奏履歴
+const PerformanceHistory = ({ slice }: { slice: PerformanceHistorySlice }) =>
+  slice.items.length == 0 ? null : (
+    <section className="pt-4 sm:pt-8 pb-8 sm:pb-12">
+      <div className="text-center p-6">
+        <div className="text-lg font-medium text-gray-900">
+          Performance History / 演奏履歴
+        </div>
       </div>
-    </div>
-    <div className="overflow-x-auto">
-      <table className="table table-compact table-auto w-full">
-        <thead>
-          <tr>
-            <th>Date (日付)</th>
-            <th>Event Name (イベント名)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {slice.items.map((row, index) => (
-            <History key={index} data={row} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  </section>
-);
+      <div className="overflow-x-auto">
+        <table className="table table-compact table-auto w-full">
+          <thead>
+            <tr>
+              <th>Date (日付)</th>
+              <th>Event Name (イベント名)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {slice.items.map((row, index) => (
+              <History key={index} data={row} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
 
 const History = ({ data }: { data: PerformanceHistorySliceDefaultItem }) => {
   const date = prismicH.asDate(data.performance_date);
@@ -49,6 +50,8 @@ const History = ({ data }: { data: PerformanceHistorySliceDefaultItem }) => {
 };
 
 function formatDate(dt: Date) {
+  if (!dt) return "";
+
   const y = dt.getFullYear();
   const m = ("00" + (dt.getMonth() + 1)).slice(-2);
   const d = ("00" + dt.getDate()).slice(-2);
